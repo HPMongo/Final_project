633309194b610a2333a3e2250b2e56a3f7fb9530
Huy Pham <phamhu@onid.oregonstate.edu>
2015-05-28 21:38:59 -0500

    Add images and index page.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Widgetstastic</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body>
+	<div class="container">
+		<div class="page-header">
+			<h1>
+				Welcome to Widgets-tastic where you can find awesome widgets at an awesomer price!
+			</h1>
+		</div>
+	</div>
+</body>
+</html>
